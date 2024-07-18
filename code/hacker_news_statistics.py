6ee91605e9060355ed77e7521981f8cadac1a5ca
Moshe Winberg <59839782+MosheRW@users.noreply.github.com>
2024-07-18 15:58:58 +0300

    main module headers functions written

--- code/hacker_news_statistics.py
+++ code/hacker_news_statistics.py
@@ -1,6 +1,105 @@
 import item
 import api
 
+def get_items_array(uids_list:list)	-> None:
+    #   TODO:    get_items_array documantation 
+    """
+        name:  item.extend_coments
+        
+        input:  
+            -   the internal index of the comment.
+            -   the  comment uid.
+            -   the new outer index of the comment.                
+        output: None.
+        
+        part:   to add the outer index of the full detailed comment in the second array
+        """
+    #   TODO:   get_items_array script
+    pass
+
+def repurpose_the_array(uids_list:list) ->  None:
+      #   TODO:    repurpose_the_array documantation 
+    """
+        name:  item.extend_coments
+        
+        input:  
+            -   the internal index of the comment.
+            -   the  comment uid.
+            -   the new outer index of the comment.                
+        output: None.
+        
+        part:   to add the outer index of the full detailed comment in the second array
+        """
+    #   TODO:   repurpose_the_array script
+    pass    
+
+def constract_comments_array(stories_list:list)    ->  list:
+    #   TODO:    constract_comments_array documantation
+    """
+        name:  item.extend_coments
+        
+        input:  
+            -   the internal index of the comment.
+            -   the  comment uid.
+            -   the new outer index of the comment.                
+        output: None.
+        
+        part:   to add the outer index of the full detailed comment in the second array
+        """
+    #   TODO:   constract_comments_array script
+    pass
+
+def statistics()    -> list:
+    #   TODO:    statistics documantation 
+    """
+        name:  item.extend_coments
+        
+        input:  
+            -   the internal index of the comment.
+            -   the  comment uid.
+            -   the new outer index of the comment.                
+        output: None.
+        
+        part:   to add the outer index of the full detailed comment in the second array
+        """
+    #   TODO:   statistics script
+    pass
+
+#   TODO:   Auxiliary functions for statistics
+
+def save_as_CSV(array:list)   ->  None:
+        #   TODO:    save_as_CSV documantation 
+    """
+        name:  item.extend_coments
+        
+        input:  
+            -   the internal index of the comment.
+            -   the  comment uid.
+            -   the new outer index of the comment.                
+        output: None.
+        
+        part:   to add the outer index of the full detailed comment in the second array
+        """
+    #   TODO:   save_as_CSV script
+    pass    
+
+def show_statistics()   -> None:
+    #   TODO:    show_statistics documantation 
+    """
+        name:  item.extend_coments
+        
+        input:  
+            -   the internal index of the comment.
+            -   the  comment uid.
+            -   the new outer index of the comment.                
+        output: None.
+        
+        part:   to add the outer index of the full detailed comment in the second array
+        """
+    #   TODO:   show_statistics script
+    pass
+
+
 
 #	Harvest the top stories uid's and place them in an array
 #	TODO:	Harvest the top stories uid's and place them in an array										<-
