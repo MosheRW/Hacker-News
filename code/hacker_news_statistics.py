410be7c7e53e5870c9e3eeec8baea38762a8bf81
Moshe Winberg <59839782+MosheRW@users.noreply.github.com>
2024-07-18 15:43:56 +0300

    design notes added to main

---
+++ code/hacker_news_statistics.py
@@ -0,0 +1,33 @@
+import item
+import api
+
+
+#	Harvest the top stories uid's and place them in an array
+#	TODO:	Harvest the top stories uid's and place them in an array										<-
+
+#	creat objects to every uid story and place it in the OG array. function
+#	TODO:	creat objects to every uid story and place it in the OG array. function							<-
+
+
+#	travers on the previous array and haverst the the comments uid's and place them in an array
+#	TODO:	travers on the previous array and haverst the the comments uid's and place them in an array		<-
+#	TODO:	find out what are the top-level comments, and how to get them									<-
+
+#	statistics:
+#	TODO:	which statistics				<-
+#	TODO:	which order						<-
+#	TODO:	how								<-
+
+
+#	CSV's files
+
+#	the stories CSV file
+#	TODO:	the stories CSV file			<-
+
+#	the comments CSV file
+#	TODO:	the stories comments file		<-
+
+#	the statistics CSV file
+#	TODO:	the stories statistics file		<-
+
+#	show the statistics on the screen
